e5317b29c669b5c9019789df62004a3249503064
xlourdes2001 <escuderolourdesmicaela@gmail.com>
2024-10-10 18:57:08 -0300

    borrar

--- src/UniversidadULP/Vistas/VistaMateria.java
+++ src/UniversidadULP/Vistas/VistaMateria.java
@@ -162,7 +162,7 @@ public class VistaMateria extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        dispose();
+       
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
